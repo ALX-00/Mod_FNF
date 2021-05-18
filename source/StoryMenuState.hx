5d66aadef8bc61a8590e0adb5d3467017fc922e5
Alejandro Valero <64314663+ALX-00@users.noreply.github.com>
2021-05-18 19:11:35 +0200

    Added things that should have been there but there weren't

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -31,11 +31,11 @@ class StoryMenuState extends MusicBeatState
 		['Satin Panties', "High", "Milf"],
 		['Cocoa', 'Eggnog', 'Winter Horrorland'],
 		['Senpai', 'Roses', 'Thorns'],
-		['Ho']
+		['ho']
 	];
 	var curDifficulty:Int = 1;
 
-	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, true,true];
+	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, true, true];
 
 	var weekCharacters:Array<Dynamic> = [
 		['', 'bf', 'gf'],
@@ -45,7 +45,7 @@ class StoryMenuState extends MusicBeatState
 		['mom', 'bf', 'gf'],
 		['parents-christmas', 'bf', 'gf'],
 		['senpai', 'bf', 'gf'],
-		['dad', 'bf','gf']
+		['dad', 'bf', 'gf']
 	];
 
 	var weekNames:Array<String> = [
@@ -56,7 +56,7 @@ class StoryMenuState extends MusicBeatState
 		"MOMMY MUST MURDER",
 		"RED SNOW",
 		"Hating Simulator ft. Moawling",
-		"Ho ft. Alva Majo"
+		"HO ft. Alva Majo"
 	];
 
 	var txtWeekTitle:FlxText;
