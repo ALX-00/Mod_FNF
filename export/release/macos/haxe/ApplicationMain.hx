6f0339a183ee4fc98d8148d855690189aeeb6728
Alejandro Valero <64314663+ALX-00@users.noreply.github.com>
2021-07-05 21:14:17 +0200

    the binary for macOS

--- export/release/macos/haxe/ApplicationMain.hx
+++ export/release/macos/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "1405";
+		app.meta["build"] = "44";
 		app.meta["company"] = "kadedev";
 		app.meta["file"] = "Kade Engine";
 		app.meta["name"] = "Friday Night Funkin': Kade Engine";
